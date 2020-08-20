0284f31da8550ac3f4a4359bfaea7ead0b9fc940
ryutaro <ryutaro@icloud.com>
2020-08-20 16:01:17 +0900

    ファイル内整理

--- app/read.rb
+++
@@ -1,8 +0,0 @@
-# now1 = Time.new; p now1  # 2017-04-25 21:01:27 +0900
-# now2 = Time.now; p now2  # 2017-04-25 21:01:27 +0900
-
-# t = Time.new
-# str = t.strftime("%Y年%m月%d日%H:%M:%S")
-# puts str  # 2017-04-25 21:45:06
-
-# t1 = Time.local(2017, 5, 1, 9, 33, 45, 0); p t1
--- app/weather2.app.rb
+++
@@ -1,7 +0,0 @@
-require "date"
-
-# 本日の日付で生成
-d1 = Date.today
-d2 = d1 + 1
-
-puts %w(日 月 火 水 木 金 土)[d2.wday] + "曜日"
